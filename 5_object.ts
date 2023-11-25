f8832f0162177d76a8cf8279120f6969992b015b
piumikaDil <piumikaDil@gmail.com>
2023-11-25 12:38:39 +0530

    5_objects

---
+++ 5_object.ts
@@ -0,0 +1,9 @@
+let student: {
+  id: number;
+  name: string;
+  age: number;
+};
+
+student = { id: 1, name: "Piumika", age: 22 };
+// student = { id: 1, name: "Piumika", age: 22,gender:"male" };   //Error
+// student = 10; // error
