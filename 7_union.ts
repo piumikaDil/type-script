4c9808c56556c3135147739babc86c17cd287cf7
piumikaDil <piumikaDil@gmail.com>
2023-12-02 10:40:51 +0530

    7_union

---
+++ 7_union.ts
@@ -0,0 +1,23 @@
+let array: (number | string | boolean)[] = [1, 2, "Hello", true];
+
+class Student {
+  id: number;
+  name: string;
+  college: string;
+  faculty: string;
+}
+
+class Teacher {
+  id: number;
+  name: string;
+  subject: string;
+  college: string;
+}
+
+class Soldier {
+  id: number;
+  name: string;
+  post: string;
+}
+
+let people: (Student | Teacher | Soldier)[] = [];
